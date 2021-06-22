1cbe0ec88eea5e0d7cbfdfb7fb1b53216a97d7d4
sathish-1996 <sathishkumarthiru2296@gmail.com>
2021-06-22 17:30:55 +0530

    changes made for municipal corporation

--- src/views/components/municipalcorporation/MunicipalCorporation.js
+++ src/views/components/municipalcorporation/MunicipalCorporation.js
@@ -273,16 +273,12 @@ const MunicipalCorporation = () => {
                     <CCol>
                       <CCol
                         md="5" 
-                        style={{
-                          marginLeft: "5px",
-                          float: "right",
-                          marginTop: "-20px",
-                        }}
+                      
                       >
                         <CButton
                           style={{
-                            float: "right",
-                            marginRight: "1365px",
+                         
+                            marginLeft: "35px",
                           }}
                           id={"saveAbbreviationConfigureCode"}
                           className={"saveBtn"}
